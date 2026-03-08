cce9d6422fcc8331b0541cde4a67065d1c50a808
Hari Raj Basnet <harirajbasnet0@gmail.com>
2026-03-08 16:29:35 +0545

    update

---
+++ src/Pages/Projects.jsx
@@ -0,0 +1,16 @@
+import Project from '../ProjectCard/ProjectCard'
+
+const Projects = () => {
+  return (
+    <div>
+        <div className="h-[200px] flex items-center justify-center relative ">
+        <h1 className=" absolute bottom-0 border-2  p-5 w-xs justify-center flex text-4xl font-bold ">PROJECTS</h1>
+      </div>
+      <div>
+<Project/>
+      </div>
+    </div>
+  )
+}
+
+export default Projects
