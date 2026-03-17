851e3e66a1a2184a50e2fcfc0f0a663c7c3200ae
Hari Raj Basnet <harirajbasnet0@gmail.com>
2026-03-17 23:26:39 +0545

    update

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -41,10 +41,10 @@ const [theme,setTheme] = useContext(ThemeData)
         <h1 className=" absolute bottom-0 border-2 p-2 md:p-5 w-3xs md:w-xs justify-center flex text-xl md:text-4xl font-bold ">CONTACT </h1>
       </div>
       <div className="w-full flex justify-center">
-<form onSubmit={submitHandler} className= "flex flex-col gap-2 md:gap-5 w-xl justify-center border-2 p-3"> 
+<form onSubmit={submitHandler} className= {`${theme ? "":"text-zinc-300 "}flex flex-col gap-2 md:gap-5 w-xl justify-center border-2 p-3`}> 
 
-  <div className="flex flex-col gap-2 p-2"> 
-   <label className="text-xl md:text-2xl font-semibold text-gray-900">Name</label>
+  <div className="flex flex-col gap-2 p-2 "> 
+   <label className="text-xl md:text-2xl font-semibold">Name</label>
   <input type="text" 
   onChange={handleChange}
   name="userName"
@@ -56,7 +56,7 @@ const [theme,setTheme] = useContext(ThemeData)
 
 
  <div className="flex flex-col gap-2 p-2 ">
-   <label className="text-xl md:text-2xl font-semibold text-gray-900">Email</label>
+   <label className="text-xl md:text-2xl font-semibold">Email</label>
   <input type="email" 
   name="email"
   onChange={handleChange}
@@ -68,19 +68,19 @@ const [theme,setTheme] = useContext(ThemeData)
 </div>
 
  <div className="flex flex-col gap-2 p-2"> 
-   <label className="text-xl md:text-2xl font-semibold text-gray-900">Phone</label>
+   <label className="text-xl md:text-2xl font-semibold ">Phone</label>
   <input type="phone" 
   name="phone"
   onChange={handleChange}
   value={formData.phone}
   placeholder="98510*****"
   required
-  className="bg-zinc-300 border-x-3 border-gray-400 font-stretch-50% text-xs md:text-2xl p-2  rounded-2xl outline-none"
+  className="bg-zinc-300 border-x-3 font-stretch-50% text-xs md:text-2xl p-2  rounded-2xl outline-none"
   />
   </div>
 
    <div className="flex flex-col gap-2 p-2"> 
-   <label className="text-xl md:text-2xl font-semibold text-gray-900">Message</label>
+   <label className="text-xl md:text-2xl font-semibold ">Message</label>
   <textarea name="message"
   value={formData.message}
   rows="4"
@@ -90,7 +90,7 @@ const [theme,setTheme] = useContext(ThemeData)
   </textarea>
   </div>
   <div className=" flex justify-center">
-    <button type="submit" className="text-xs md:text-2xl text-gray-950 bg-amber-600 p-3 rounded-b-2xl rounded-t-xl">Submit</button>
+    <button type="submit" className="text-xs md:text-2xl  bg-amber-600 p-3 rounded-b-2xl rounded-t-xl">Submit</button>
   </div>
 </form>
       </div>
